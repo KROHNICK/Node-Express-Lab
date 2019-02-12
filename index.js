afd31e1cac9b950e84cd7284d7bd4341bc75de8e
KROHNICK <business.kanekperez@gmail.com>
2019-02-12 12:45:01 -0800

    Completed Put Endpoint

--- index.js
+++ index.js
@@ -74,6 +74,37 @@ server.delete("/api/posts/:id", (req, res) => {
     });
 });
 
+server.put("/api/posts/:id", (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
+  db.update(req.params.id, req.body)
+    .then(post => {
+      if (!post) {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist."
+        });
+      } else {
+        db.findById(req.params.id)
+          .then(updated => {
+            res.status(200).json(updated);
+          })
+          .catch(err => {
+            res.status(404).json({
+              error: "Post not found after updating."
+            });
+          });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "The post information could not be modified."
+      });
+    });
+});
+
 server.listen(4000, () => {
   console.log("\n*** Server running on http://localhost:4000 ***\n");
 });
